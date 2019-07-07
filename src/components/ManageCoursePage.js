5609d98488f2bb5a406793a4a590b72b86f6c844
wil.i.am <wiliam.tae@gmail.com>
2019-07-07 12:33:20 -0300

    promt test added to meanage courses page

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Courses</h2>
+      <Prompt when={true} message="Are yout sure you want to leave?" />
       {props.match.params.slug}
     </>
   );
