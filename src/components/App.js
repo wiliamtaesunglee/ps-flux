1c20036cf86229309d9f60aac6176fef2c3f3b94
wil.i.am <wiliam.tae@gmail.com>
2019-07-07 11:46:20 -0300

    404 page and switch added

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import NotFoundPage from "./NotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
