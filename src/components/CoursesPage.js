3a9058b259dbe185c7e4e4f0d06cb87ba0597279
wil.i.am <wiliam.tae@gmail.com>
2019-07-06 21:16:33 -0300

    components added to project and some components to

---
+++ src/components/CoursesPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    getCourses().then(courses => this.setState({ courses: courses }));
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+        </table>
+      </>
+    );
+  }
+}
+
+export default CoursesPage;
---
+++ src/components/common/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/courses">courses</a> |{" "}
+      <a href="/about">About</a>
+    </nav>
+  );
+}
+
+export default Header;
