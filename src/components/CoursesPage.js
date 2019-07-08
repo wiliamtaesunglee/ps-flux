cea61c8aee1636a90d030f92b9b238906b15464f
wil.i.am <wiliam.tae@gmail.com>
2019-07-08 12:06:03 -0300

    delete btn added

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import courseStore from "../stores/courseStore";
 import CourseList from "./CourseList";
 import { Link } from "react-router-dom";
-import { loadCourses } from "../actions/courseActions";
+import { loadCourses, deleteCourse } from "../actions/courseActions";
 function CoursesPage() {
   const [courses, setCourses] = useState(courseStore.getCourses());
 
@@ -22,7 +22,7 @@ function CoursesPage() {
       <Link className="btn btn-primary" to="/course">
         Add Courses
       </Link>
-      <CourseList courses={courses} />
+      <CourseList courses={courses} deleteCourse={deleteCourse} />
     </>
   );
 }
