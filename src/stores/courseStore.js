2f794ab33ccf4b6993160a47d2a96110bea7e981
wil.i.am <wiliam.tae@gmail.com>
2019-07-08 11:13:39 -0300

    stores initialization

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -35,6 +35,10 @@ Dispatcher.register(action => {
       _courses.push(action.course);
       store.emitChange();
       break;
+    case actionTypes.LOAD_COURSES:
+      _courses = action.courses;
+      store.emitChange();
+      break;
     default:
     // nothig to do here
   }
